0a41ab990a43cf1e332781db69e09ef458ebc409
Josaphat <josa1980@gmail.com>
2020-02-20 20:04:55 -0600

    Creacion del Modelo de Articulos

---
+++ Models/articulos_Model.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ *@autor Josaphat Lopez 
+ *Modelo para Articulos
+ */
+class articulos_Model
+{
+	private $db;
+	private $articulos;
+	function __construct(argument)
+	{
+		require_once("/modelo/dbConector.php");
+		$this->db=dbConector::conexion();
+		$this->articulos=array();
+
+	}
+
+	public function get_articulos(){
+		$consulta=$this->db->query("SELECT * FROM articulos WHERE estado=1");
+		while($filas=$consulta->fetch(PDO::FETCH_ASSOC)){
+			$this->articulos[]=$filas;
+		}
+		return $this->articulos;
+	}
+}
+
+
+
+ ?>
