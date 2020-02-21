5f42cedf99fc386a84ad74235d49eb6ca2d1b6e2
Josaphat <josa1980@gmail.com>
2020-02-20 21:45:49 -0600

    agregando el diseño
    se agrega el diseño de crud

--- Views/articulos_View.php
+++ Views/articulos_View.php
@@ -1,21 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Articulos</title>
-</head>
-<body>
-	<table>
-		<tr>
-		<th colspan="3">Articulos</th>			
-		</tr>
-		<tr><th>Codigo</th><th>Nombre del Articulo</th><th>Precio</th></tr>
-		<tr><td></td><td></td><td></td></tr>
+
+
+	<DIV class="col-12 center">
+		
+<h3>Articulos</h3>
+	</DIV>
+<button type='button' class="btn btn-success" >Agregar</button>
+<div class="row">
+	<div class="col-12">
+	<table id="tablaArticulos" class="display" style="width:100%">
+		<thead>		
+		
+		<tr><th>Codigo</th><th>Nombre del Articulo</th><th>Precio</th><th>Accion</th></tr>
+		</thead>
+		<tbody>
+			
 		<?php 
 			foreach ($listadoProductos as $row) {
-				echo "<tr><td>".$row["idArticulo"]."</td><td>".$row["descripcion"]."</td><td>".$row["precioVenta"]."</td></tr>";
+				echo "<tr><td>".$row["idArticulo"]."</td><td>".$row["descripcion"]."</td><td>".$row["precioVenta"]."</td><td><button type='button' class='btn btn-outline-primary'>Editar</button><button type='button'>Eliminar</button></td></tr>";
 			}
 		 ?>
+		</tbody>
 	</table>
+	</div>
+</div>
 
-</body>
-</html>
--- index.php
+++ index.php
@@ -3,10 +3,27 @@
 <head>
 	<meta charset="utf-8">
 	<title>Modelo Vista Controlador CRUD</title>
+	<!--Estilos-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css"/>
 </head>
 <body>
 <?php 
 	require_once("controllers/articulos_Controller.php");
  ?>
+ <script
+  src="https://code.jquery.com/jquery-3.0.0.js"
+  integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo="
+  crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$(document).ready( function () {
+    $('#tablaArticulos').DataTable();
+} );
+
+</script>
+
 </body>
 </html>
