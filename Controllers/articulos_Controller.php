174ad0f7805b127c2b990af4899ebce5ad542b36
Josaphat <josa1980@gmail.com>
2020-02-20 20:12:29 -0600

    creacion del Controlador de Articulos

---
+++ Controllers/articulos_Controller.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * @autor Josaphat Lopez Ceron
+ *Controlador de Articulos
+ */
+//LLamada al Modelo
+require_once("/Models/articulos_Model.php");
+$productos=new articulos_Model();
+$listadoProductos=$productos->get_articulos();
+
+//Llamada a la vista
+require_once("/Views/articulos_View.php");
+
+ ?>
