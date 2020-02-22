46385dd5a8c86e24a59f34c51090304519b73237
Josaphat <josa1980@gmail.com>
2020-02-21 20:58:34 -0600

    se agrega  Modal
    se agrega Modal para Agregar Articulo

--- Views/articulos_View.php
+++ Views/articulos_View.php
@@ -6,7 +6,7 @@
 	</DIV>
 <div class="row">
 		<div class="col-md-12 d-flex justify-content-end">			
-		<button type='button' class="btn btn-success" >Agregar</button>
+		<button type='button' class="btn btn-success" data-toggle="modal" data-target="#modalCRUD" >Agregar</button>
 		</div>
 		<br/>
 	<div class="col-md-12">
@@ -28,3 +28,35 @@
 	</div>
 </div>
 
+<!----Modal para CRUD--->
+
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="modalCRUD" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Agregar Articulo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+        	<div class="form-group">
+        		<label for="descripcion">Producto:</label>
+    				<input type="text" class="form-control" id="descripcion" aria-describedby="descripcion" placeholder="Ingrese el Articulo">
+    			<label for="costo">Costo:</label>
+    				<input type="text" class="form-control" id="costo" aria-describedby="costo" placeholder="0.000">
+    				<label for="precio">Precio de Venta:</label>
+    				<input type="text" class="form-control" id="precio" aria-describedby="precio" placeholder="0.000">
+        	</div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success">Guardar</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!----END Modal para CRUD--->
